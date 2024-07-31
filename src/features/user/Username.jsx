8ada0d22d420f3a3b8abe794898bbb32802f4583
youssef-khalifa <ykhalifa19921@gmail.com>
2024-07-31 23:17:48 +0300

    s

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
   return (
-    <div className="uppercase text-sm font-medium tracking-widest hidden md:block">
-      youssef
-    </div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
